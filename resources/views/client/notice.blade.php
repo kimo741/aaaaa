e3843494725e004b75d35b0f3f2f678402f2469b
Ahmed <abdelsalama222@gmail.com>
2022-04-21 14:23:46 +0200

    Ahmed to Dash

--- resources/views/client/notice.blade.php
+++ resources/views/client/notice.blade.php
@@ -2,21 +2,23 @@
 @extends('admin::layouts.anonymous-master')
 
 @section('content')
+                <h1>E-mail Verification</h1>
     <div class="panel">
-        <div class="panel-body">
+        <div class="panel-body" style="padding: 25px">
 
             <div class="form-container">
-                <h1>E-mail Verification</h1>
-                <p>Check Your E-mail To Verify Your Account...</p>
-                <small>You can't login without verifing your account !!
-                    <div class="spinner-border spinner-border-sm text-success" role="status">
-                        <span class="visually-hidden">Checking...</span>
-                    </div>
+            <h1 style="margin-bottom:21px">Hi, {{ $client->first_name }}</h1>
+                <p style="line-height: 2;font-size: 15px;">
+                    plece Check Your E-mail send To
+                    <strong>
+                        {{ $client->email }}
+                    </strong>
+                </p>
+                <small style="font-size:12px">You can't login without verifing your account</small>
+                <small style="font-size:12px">If you have problem pleac contact-us at
+                    <a style="display: inline;" href="mailto:admin@ladune.com">admin@ladune.com</a>
                 </small>
-                <form class="mt-3" method="POST" action="{{route('verification.send')}}">
-                    <p>You Doesn't Get E-mail ? </p>
-                    <button type="submit" class="btn btn-primary-outline">Send Mail Again</button>
-                </form>
+                <a href="{{route('verification.send', $client->email)}}" class="mb-0 mt-5">Resend email </a>
             </div>
         </div>
     </div>
